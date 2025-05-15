97a6e4882703c3371d40827c85ce27f7a53107df
StijnVM6 <stijn.vm@hotmail.com>
2025-05-15 09:33:42 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,28 @@
 import { useEffect, useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
 	const [message, setMessage] = useState("Loading...");
 
 	useEffect(() => {
-		fetch("/api/hello")
+		fetch("/api")
 			.then((res) => res.json())
 			.then((data) => setMessage(data.message))
 			.catch((err) => setMessage("Error: " + err.message));
 	}, []);
 
-	return <h1>{message}</h1>;
+	return (
+		<>
+			<div>
+				<img src={viteLogo} className="logo" alt="Vite logo" />
+				<img src={reactLogo} className="logo react" alt="React logo" />
+			</div>
+			<h1>Vite + React</h1>
+			<h2>{message}</h2>
+		</>
+	);
 }
 
 export default App;
-
-// function App() {
-// 	const [message, setMessage] = useState("Loading...");
-
-// 	useEffect(() => {
-// 		fetch("/")
-// 			.then((res) => res.json())
-// 			.then((data) => setMessage(data.message))
-// 			.catch((err) => setMessage("Error: " + err.message));
-// 	}, []);
-
-// 	return (
-// 		<>
-// 			<div>
-// 				<a href="https://vite.dev" target="_blank">
-// 					<img src={viteLogo} className="logo" alt="Vite logo" />
-// 				</a>
-// 				<a href="https://react.dev" target="_blank">
-// 					<img src={reactLogo} className="logo react" alt="React logo" />
-// 				</a>
-// 			</div>
-// 			<h1>Vite + React</h1>
-// 			<h2>{message}</h2>
-// 		</>
-// 	);
-// }
