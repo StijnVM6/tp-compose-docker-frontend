d1c52a59fe9b36152d76ef7a8fd5422c1f216fa7
StijnVM6 <stijn.vm@hotmail.com>
2025-05-15 09:38:10 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+const apiUrl = import.meta.env.VITE_API_URL || "";
 
 function App() {
 	const [message, setMessage] = useState("Loading...");
 
 	useEffect(() => {
-		fetch("/api")
+		fetch(`${apiUrl}/api`)
 			.then((res) => res.json())
 			.then((data) => setMessage(data.message))
 			.catch((err) => setMessage("Error: " + err.message));
